a527dd1d4608640a83ff3a6016ea105344e4bfff
Michael Dangcil <mdangcil@rapiddisplays.com>
2019-07-30 10:06:55 -0700

    modified: js/main.js and js/restaurant_info.js
    Added in

--- js/main.js
+++ js/main.js
@@ -160,9 +160,9 @@ createRestaurantHTML = (restaurant) => {
 
   const image = document.createElement('img');
   image.className = 'restaurant-img';
+  image.alt = 'Photo of ' + restaurant.name + ' restaurant';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
   li.append(image);
-
   const name = document.createElement('h1');
   name.innerHTML = restaurant.name;
   li.append(name);
